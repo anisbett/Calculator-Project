aea18badadd0a3fc50128a7f9f2f13bfb2f6a431
anisbett <ajaninisbett@outlook.com>
2023-03-23 20:17:45 -0400

    Project completed

--- addStyles.js
+++ addStyles.js
@@ -6,7 +6,7 @@ const calcBodyClass = "max-w-lg rounded overflow-hidden shadow-2xl bg-pink-700 b
 const calcBody = document.querySelector('.calcBody');
 calcBodyClass.split(" ").forEach((cls) => calcBody.classList.add(cls));
 
-const outputClass = "flex flex-col 2-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border gray-500 rounded";
+const outputClass = "flex flex-col w-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border gray-500 rounded";
 const output = document.querySelector('.output');
 outputClass.split(" ").forEach((cls) => output.classList.add(cls));
 
@@ -18,6 +18,31 @@ const currentNumberClass = "flex max-w-lg h-2/3 pb-2 text-xl";
 const currentNumber = document.querySelector('.currentNumber');
 currentNumberClass.split(" ").forEach((cls) => currentNumber.classList.add(cls));
 
-const clearClass = "flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl hover:bg-red-500";
-const clear = document.querySelector('.clear');
-clearClass.split(" ").forEach((cls) => clear.classList.add(cls));
+const buttons = document.querySelectorAll('button');
+buttons.forEach((btn) => {
+    addClasses(btn)
+})
+
+function addClasses(button) {
+    const btnNumberClass = "hover:bg-green-500 border-green-500";
+    const btnClearClass = "hover:bg-orange-500 border-orange-500";
+    const btnOpClass = "hover:bg-blue-500 border-blue-500";
+    const btnEqClass = "hover:bg-red-500 border-red-500";
+    const btnDecClass = "hover:bg-yellow-500 border-yellow-500";
+
+    if (button.classList.value === "number") {
+        btnNumberClass.split(" ").forEach((cls) => button.classList.add(cls));
+    } else if (button.classList.value === "operator") {
+        btnOpClass.split(" ").forEach((cls) => button.classList.add(cls));
+    } else if (button.classList.value === "clear") {
+        btnClearClass.split(" ").forEach((cls) => button.classList.add(cls));
+    } else if (button.classList.value === "equal") {
+        btnEqClass.split(" ").forEach((cls) => button.classList.add(cls));
+    } else if (button.classList.value === "decimal") {
+        btnDecClass.split(" ").forEach((cls) => button.classList.add(cls));
+    }
+
+    const baseClass = "flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl";
+    baseClass.split(" ").forEach((cls) => button.classList.add(cls));
+}
+
